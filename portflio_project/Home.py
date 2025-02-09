e570913abef867d8d5512005a179aafdd14e8fd1
sriram-ravi705 <sriramravi477@gmail.com>
2025-02-09 12:10:49 +0530

    day-23

--- portflio_project/Home.py
+++ portflio_project/Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -15,3 +16,19 @@ with col2:
     st.info(content)
 
 st.write("below you can find some of the app i have build in python, feel free to contact me,below you can find some of the app i have build in python, feel free to contact me")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f'images/{row['image']}')
+        st.write(f"[source code]({row["url"]})")
+
+with col4:
+    for index,row in df[11:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f'images/{row['image']}')
